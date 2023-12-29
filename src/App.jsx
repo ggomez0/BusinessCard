80be5d7ea24c96fe84bbf05434c47e4e8ab2d43b
ggomez0 <gaspargomez2000@outlook.com>
2023-12-28 21:17:10 -0300

    Ui Container

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-[#C8E6CA] h-screen flex justify-center items-center">
+      <div className='bg-white p-5 rounded-3xl shadow-xl h-3/6 w-3/6' >
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
