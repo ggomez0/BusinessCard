63c378a4f7c4ee9956bab08f211a529ff74e3af1
ggomez0 <gaspargomez2000@outlook.com>
2023-12-28 23:00:46 -0300

    User Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="bg-[#C8E6CA] h-screen flex justify-center items-center">
-      <div className='bg-white p-5 rounded-3xl shadow-xl h-3/6 w-3/6' >
-        
+      <div className='bg-white rounded-3xl shadow-xl h-3/4 w-3/4' >
+        <div className='bg-green-600 max-w-xs  h-[68%] rounded-tl-3xl mb-3 text-white pl-10 pt-60 '>
+          <h2 className='font-bold'>✉ Email</h2>
+          <p>gaspargomez2000@outlook.com</p>
+          <br />
+          <h3 className='font-bold'>📱 Celular</h3>
+          <p>(+54) 387 1234567</p>
+
+        </div>
+        <div className='bg-green-600 max-w-xs h-[30%] rounded-bl-3xl pt-5 pl-10 text-white flex flex-col'>
+          <h3>Me podés encontrar en</h3>
+          <div className='flex'>
+          <a href="https://github.com/ggomez0" >
+            <img src="/src/assets/github.svg" className='w-8' alt="GitHub"/>
+          </a>
+
+          <a href="https://linkedin.com/in/gaspargomez" >
+            <img src="/src/assets/linkedin.svg" className='w-8' alt="LinkedIn"/>
+          </a></div>
+        </div>
+
       </div>
     </div>
   )
